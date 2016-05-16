00453bba77fa13d2db1076016ad99ad2135d0a8a
pavelnovotny <pavel.novotny.cz@gmail.com>
2016-05-16 10:17:12 +0200

    Přidání express.js routování

---
+++ server/server.js
@@ -0,0 +1,37 @@
+/**
+ *
+ * Created by pavelnovotny on 12.05.16.
+ */
+
+var express = require("express");
+var path = require("path");
+var cors = require("cors");
+var app = express();
+var request = require("request");
+
+app.use(cors());
+
+app.get("/app", function(req, res) {
+    res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+});
+app.use(express.static(path.join(__dirname, '../dist')));
+
+// app.get("/app", function(req, res) {
+//     res.sendFile(path.join(__dirname, '../client', 'index.html'));
+// });
+// app.use(express.static(path.join(__dirname, '../client')));
+//app.use(express.static(path.join(__dirname, '../.tmp')));
+
+//proxy that does not wait for response finish (feeds browser immediatelly)
+var proxied = ['/processCommand','/download','/hashSeek','/authenticate','/result','/sessions','/logFilesInfo'];
+
+app.use(function(req, res) {
+    if (proxied.indexOf(req.path) != -1) {
+        var newurl = 'http://lxcipppt401.ux.to2cz.cz:7002' + req.originalUrl;
+        req.pipe(request[req.method.toLowerCase()](newurl)).pipe(res);
+    }
+});
+
+app.listen(8000).on('error', function(err) {
+    console.error(err);
+});
