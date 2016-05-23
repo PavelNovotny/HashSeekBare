69bb35afe9b27095adf766d4d86e264dca814874
pavelnovotny <pavel.novotny.cz@gmail.com>
2016-05-23 13:37:26 +0200

    zmeny startovaciho a stop scriptu

--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+npm install >> deploy.log
 echo "$(date): starting server " $1 >> deploy.log
 if [[ -n "$1" ]]; then
     nohup node server/server.js --env $1 > Output.out 2> Error.err < /dev/null &
